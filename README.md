e6168aa7f0fd71f767ad9c315a8cd7207c28995e
saknarin <saknarin.css57@ubru.ac.th>
2020-11-24 15:00:51 +0700

    Creating Documents and Testing the Model

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ npm i mongoose@version
 
 mongoDB_v2
 Creating a Simp;e Tour Model
+
+mongoDB_v3
+Creating Documents and Testing the Model
+
+mongoDB_v4
+Refactoring for MVC
--- server.js
+++ server.js
@@ -23,14 +23,30 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: true
   },
-  rating: Number,
-  Price: {
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
     type: Number,
     required: [true, 'A tour must have a price']
   }
 });
 
-const tour = mongoose.model('Tour', tourSchema);
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park camper',
+  price: 997
+});
+
+testTour
+.save()
+.then(doc => {
+  console.log(doc);
+}).catch(err => {
+  console.log('ERROR:', err);
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
