4af757dc7c90996ac2b7da8a0cdf6f4c57262a1d
saknarin <saknari.ccs57@ubru.ac.th>
2022-05-16 13:56:39 +0700

    Implementing a Global Error Handling Middleware

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const AppError = require('./utils/appError');
-const globalErrorHandler = require('./utils/globalError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
